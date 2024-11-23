6496f885db1f2b320bb94712da6382c7fe22efd5
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2024-11-23 18:25:01 +0100

    WIP

--- src/pages/organization/projects.js
+++ src/pages/organization/projects.js
@@ -1,10 +1,15 @@
 // src/pages/index.js
 import React from 'react';
+import Table from '@/components/Table';
 
 export default function Projects() {
   return (
-    <div>
-      <p>This is the projects page.</p>
-    </div>
+    <>
+    <h1>Core projects involving ORFEUS</h1>
+    <h2>Ongoing projects</h2>
+    <Table apiPath="/api/projects?state=ongoing" fields={["name", "years", "description"]} />
+    <h2>Finalized projects</h2>
+    <Table apiPath="/api/projects?state=finalized" fields={["name", "years", "description"]} />
+    </>
   );
 }
